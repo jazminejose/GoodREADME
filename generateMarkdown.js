81590c1df59c5ff18d7394319ce69b054e07c5d7
jazminejose <jazzi.lyn217@gmail.com>
2021-12-06 19:13:21 -0800

    set array of questions

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -2,6 +2,39 @@
 function generateMarkdown(data) {
   return `# ${data.title}
 
+## Table of Content(s)
+
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Description
+${data.description}
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## Contributing
+${data.contributing}
+
+## Tests
+${data.tests}
+
+## License
+${data.license}
+
+## Questions
+For any questions, please contact me with the information below:
+Github: https://github.com/${data.username}
+Email: ${data.email}
+
 `;
 }
 
--- index.js
+++ index.js
@@ -1,16 +1,75 @@
+const inquirer = require('inquirer');
+const axios = require('axios');
+const fs = require('fs');
+const generateMarkdown = require('./generateMarkdown.js');
+
 // array of questions for user
 const questions = [
-
+{
+  type: 'input',
+  message: 'Enter your GitHub username: ',
+  name: 'username'
+},
+{
+  type: 'input',
+  message: 'Enter your project title: ',
+  name: 'repository',
+},
+{
+  type: 'input',
+  message: 'Write a description of your project: ',
+  title: 'description',
+},
+{ 
+  type: 'input',
+  message: 'Installation: ',
+  name: 'installation',
+},
+{
+  type: 'input',
+  message: 'Usage of the project: ',
+  name: 'usage'
+}
+{
+  type: 'list',
+  message: "Select License used",
+  choices: ['MIT License', 'The Unlicense', 'Google 2.0v'],
+  name: 
+},
+{
+  type: 'input',
+  message: 'Contributing information: ',
+  name: ''
+}
+{
+  type: 'input',
+  messages: "Provide your email: ",
+  name: 'email'
+}
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, err => {
+    if (err) {
+      return console.log(err);
+    }
+    console.log("Success! Your README.md file has been generated")
+  });
 }
 
 // function to initialize program
-function init() {
+async function init() {
+  try {
+    const userResponses = await inquirer.prompt(questions);
 
-}
+    const markdown = generateMarkdown(userResponses)
+
+    await writeToFile('example_README.md', markdown);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 // function call to initialize program
 init();
